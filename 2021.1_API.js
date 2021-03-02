585d4941c0d32b0a11bc67ada296bc7c7d5d80c4
Harish Bakka <harish.tech.in05@gmail.com>
2021-03-01 19:35:31 -0800

    Update 2021.1_API.js

--- 2021.1_API.js
+++ 2021.1_API.js
@@ -3,3 +3,8 @@ CurrentRecord.getSubrecord(options);
 //longer possible to use this method to check whether a record has an existing subrecord. 
 //Use the
 CurrentRecord.hasSubrecord(options); //method to determine if the field contains a subrecord.
+
+define(['N/suiteAppInfo']); 
+//new Module includes methods that let you access information about installed bundles and SuiteApps. These methods let you determine if a bundle
+//or SuiteApp is installed, retrieve a list of all installed bundles and SuiteApps, or retrieve a list of bundles
+//and SuiteApps that contain certain scripts.
